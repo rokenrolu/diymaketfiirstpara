255a9c73f76743c4ec0118a256a44b511ef6f834
19 <72027720+rokenrolu@users.noreply.github.com>
2026-09-19 11:12:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let nikita = 19
+console.log(nikita)
+const dmitriev = "nikita"
+console.log(dmitriev)
+let obekt = {
+    name: "legenda",
+    age: 19,
+    diploma: {
+        math: 2,
+        chemistry: 5, 
+        eng: 12
+    }
+}
+console.log(obekt)
+let art = ["pochan", 67, "monster"]
+console.log(art)
+let bolin = false
+// if (bolin == true){
+//     console.log(true)
+// } else{
+//     console.log(false)
+// }
+let number = 4
+let string = "4"
+if (number == string){
+    console.log(true)
+} else{
+    console.log(false)
+}
